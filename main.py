a0d78643918818a7fe796ca09233931c0a63b79f
Jules Sorensen <julessorens@gmail.com>
2023-07-12 14:14:50 +0200

    push

--- main.py
+++ main.py
@@ -4,14 +4,26 @@ import http.client
 import base64
 import ssl
 
-PORT = 9999
-
-
 class ProxyHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
+    def do_CONNECT(self):
         camouflaged_url = "/" + base64.urlsafe_b64encode(self.path.encode()).decode()
 
         conn = http.client.HTTPConnection('51.103.110.178', 443)
+        # conn = http.client.HTTPConnection('127.0.0.1', 9998)
+        conn.request("CONNECT", camouflaged_url)
+        response = conn.getresponse()
+
+        self.send_response(response.status)
+        self.send_header("Content-type", response.getheader("Content-type"))
+        self.end_headers()
+        self.wfile.write(response.read())
+
+
+    def do_GET(self):
+        camouflaged_url = "/" + base64.urlsafe_b64encode(self.path.encode()).decode()
+
+        # conn = http.client.HTTPConnection('51.103.110.178', 443)
+        conn = http.client.HTTPConnection('127.0.0.1', 9998)
         conn.request("GET", camouflaged_url)
         response = conn.getresponse()
 
@@ -20,6 +32,7 @@ class ProxyHandler(http.server.SimpleHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(response.read())
 
+PORT = 9999
 
 with socketserver.TCPServer(("", PORT), ProxyHandler) as httpd:
     print("serving at port", PORT)
